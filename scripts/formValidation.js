7db44ce5220611c688af1f7e11fd52f71d98815c
Harjit Kainth <harjitkainth10@gmail.com>
2021-09-27 10:38:06 -0700

    added error message element to html and styled by css. JS handles display of error messages

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,2 +1,6 @@
 const form = document.getElementById('register-form');
 const errorMessages = document.getElementById('error-messages');
+
+errorMessages.style.display = 'none';
+
+form.addEventListener('submit', validateForm);
